e7ce6dfbda08bdbc6e7a564b2b156eea5ee415bb
Zach Ruiz <powersbooth10@gmail.com>
2022-09-01 18:45:28 -0500

    commit tree 515a225b04b7440b893165e3aaa372ca49fc79e1

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+//take in array and a string
+//return a matching list of drivers (case-sensitive)
+
+function findMatching(drivers, str) {
+    const answer = drivers.filter(driver => driver.toLowerCase() === str.toLowerCase());
+    return answer;
+}
+
+// takes in a na array and str 
+// returns all drivers whose names start with string 
+function fuzzyMatch(drivers, str) {
+    const answer = drivers.filter(driver => driver.slice(0, 2) === str)
+    return answer
+}
+
+// takes in array and string 
+
+function matchName (drivers, str) {
+    const answer = drivers.filter(driver => driver.name === str)
+    return answer
+}
